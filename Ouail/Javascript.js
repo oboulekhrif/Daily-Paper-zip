75a840349b490d6726bf86a25f3a0a7f4aaa70ee
oboulekhrif <70693866+oboulekhrif@users.noreply.github.com>
2021-12-15 16:47:52 +0100

    1

--- Ouail/Javascript.js
+++ Ouail/Javascript.js
@@ -36,5 +36,32 @@ const openModal = (modal) => {
     alert("Weet je het zeker dat je wilt betalen?");
   }
 
+// Set the date we're counting down to
+const countDownDate = new Date("dec 31, 2021 00:00:00").getTime();
 
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = days + " DAG " + hours + " UUR "
+  + minutes + " MIN " + seconds + " SEC ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
   
